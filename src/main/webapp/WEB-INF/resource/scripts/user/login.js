811b3471fd28d6f1d20a402a8dfdd77f9b6e1b02
sengwanchoi <wan7523376@naver.com>
2021-05-14 11:53:41 +0900

    commit

--- src/main/webapp/WEB-INF/resource/scripts/user/login.js
+++ src/main/webapp/WEB-INF/resource/scripts/user/login.js
@@ -1,12 +1,20 @@
 window.addEventListener('DOMContentLaded', ()=> {
    const loginForm = window.document.getElementById('login-form');
    const emailWarning = loginForm.querySelector('[rel="email-warning"]');
+   const passwordWarning = loginForm.querySelector('[rel="password-warning"]');
    loginForm.onsubmit = () => {
-      if (loginForm['email'].value === '') {
+      if (loginForm['email'].value === '' || loginForm['email'].value === null) {
          emailWarning.classList.add('visible');
          loginForm['email'].focus();
       } else {
          emailWarning.classList.remove('visible');
+      }
+
+      if (loginForm['password'].value === '' || loginForm['password'].value === null) {
+         passwordWarning.classList.add('visible');
+         loginForm['password'].focus();
+      } else {
+         passwordWarning.classList.remove('visible');
       }
    }
 });
