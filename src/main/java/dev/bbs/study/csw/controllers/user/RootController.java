60c7344717ea904ee9f5495d1f348ca2315f8714
SeongUn <mysun218@gmail.com>
2021-06-07 17:25:04 +0900

    jsp

--- src/main/java/dev/bbs/study/csw/controllers/user/RootController.java
+++ src/main/java/dev/bbs/study/csw/controllers/user/RootController.java
@@ -1,15 +1,14 @@
 package dev.bbs.study.csw.controllers.user;
 
-import dev.bbs.study.csw.dtos.UserDto;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+
 
 @Controller
 @RequestMapping(value = "/")
-@SessionAttributes(UserDto.NAME)
 public class RootController {
     @RequestMapping(
             value = "/",
